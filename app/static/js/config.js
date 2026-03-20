9305f90238073e3658d379bf36daba0f3251497e
Kalle Tolonen <kalle.tolonen@gmail.com>
2026-03-20 18:58:06 +0200

    Make history toggles universal

--- app/static/js/config.js
+++ app/static/js/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '2.9';
+export const APP_VERSION = '3.0';
 export const API_BASE = location.origin;
 export const WS_URL = (location.protocol === 'https:' ? 'wss://' : 'ws://') + location.host + '/ws';
 export const STALE_MINUTES = 30;
